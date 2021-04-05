b6014f3af3004cd695280201f1bbfcf2dbd25f58
erikkt <60917284+erikkt@users.noreply.github.com>
2021-04-05 16:22:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -84,20 +84,20 @@ class Geekworm_LED_ring(MycroftSkill):
 	def handle_listener_off(self, message):
 		self.log.info("Pixel Ring: Off")
 		#pixel_ring.off()
-		self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-		self.led.start()
+		#self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+		self.led.stop()
 
 	def handle_listener_think(self, message):
 		self.log.info("Pixel Ring: Think")
 		#pixel_ring.think()
-		#MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-                #MY_CYCLE.start()
+		self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+		self.led.start()
 
 	def handler_listener_speak(self, message):
 		self.log.info("Pixel Ring: Speak")
 		#pixel_ring.speak()
-		#MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-                #MY_CYCLE.start()
+		self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+		self.led.start()
 
 	@intent_handler(IntentBuilder("").require("EnablePixelRing"))
 	def handle_enable_pixel_ring_intent(self, message):
