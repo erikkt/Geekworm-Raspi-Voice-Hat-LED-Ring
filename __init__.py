cec88150ac21db9046e7cddfea1a533b085040b9
erikkt <60917284+erikkt@users.noreply.github.com>
2021-04-05 15:49:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -78,26 +78,26 @@ class Geekworm_LED_ring(MycroftSkill):
 	def handle_listener_wakeup(self, message):
 		self.log.info("Pixel Ring: Wakeup")
 		#pixel_ring.listen()
-		MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-                MY_CYCLE.start()
+		self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+                self.start()
 
 	def handle_listener_off(self, message):
 		self.log.info("Pixel Ring: Off")
 		#pixel_ring.off()
-		MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-                MY_CYCLE.start()
+		self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+                self.start()
 
 	def handle_listener_think(self, message):
 		self.log.info("Pixel Ring: Think")
 		#pixel_ring.think()
-		MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-                MY_CYCLE.start()
+		#MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+                #MY_CYCLE.start()
 
 	def handler_listener_speak(self, message):
 		self.log.info("Pixel Ring: Speak")
 		#pixel_ring.speak()
-		MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
-                MY_CYCLE.start()
+		#MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=5, mosi=MOSI, sclk=SCLK)
+                #MY_CYCLE.start()
 
 	@intent_handler(IntentBuilder("").require("EnablePixelRing"))
 	def handle_enable_pixel_ring_intent(self, message):
