fbd8d9c9b4b1864f32b16865a0ed4a9f3164f4e3
erikkt <60917284+erikkt@users.noreply.github.com>
2021-04-06 11:40:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -96,7 +96,7 @@ class Geekworm_LED_ring(MycroftSkill):
 	def handle_listener_think(self, message):
 		self.log.info("Pixel Ring: Think")
 		#pixel_ring.think()
-		self.led = colorschemes.Rainbow(stop=False, num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=1, mosi=MOSI, sclk=SCLK)
+		self.led = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0, order='rgb', num_steps_per_cycle=255, num_cycles=1, mosi=MOSI, sclk=SCLK)
 		self.led.start()
 		#self.led = apa102.APA102(num_led=NUM_LED, order='rgb', mosi=MOSI, sclk=SCLK)
 		#self.led.clear_strip()
